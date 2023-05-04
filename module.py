fbbe407a06b180bd2a4bedce10841f081759b754
Nate <nathanaelaltemar@gmail.com>
2023-05-04 18:15:09 -0400

    first commit

---
+++ module.py
@@ -0,0 +1,7 @@
+length = int(input("enter length of square: "))
+width = int(input("Enter width of square: "))
+length * width == Area
+umit = input("sqaure unit")
+surfaceArea = 6 * (length ** 2)
+print(surfaceArea)
+print("the area of the square", length,width,"=", Area)
